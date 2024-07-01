064a2a80ac05c7efc7420396afa1075089ec5f6c
jschwind <info@juergen-schwind.de>
2024-07-01 11:11:59 +0200

    added more filetypes

--- src/Twig/AssetsComposerExtension.php
+++ src/Twig/AssetsComposerExtension.php
@@ -3,7 +3,6 @@
 namespace JBSNewMedia\AssetComposerBundle\Twig;
 
 use JBSNewMedia\AssetComposerBundle\Service\AssetComposer;
-use mysql_xdevapi\SqlStatementResult;
 use Twig\Extension\AbstractExtension;
 use Twig\Markup;
 use Twig\TwigFunction;
@@ -43,28 +42,28 @@ class AssetsComposerExtension extends AbstractExtension
         }
     }
 
-    public function renderStylesheets(string $position = 'all', string $prefix = '', string $suffix = ''): Markup
+    public function renderStylesheets(string $position = 'all'): Markup
     {
         $stylesheets = '';
         if ((isset($this->assets[$position])) && (isset($this->assets[$position]['css'])) && ([] !== $this->assets[$position]['css'])) {
             foreach ($this->assets[$position]['css'] as $assetFilename) {
-                $stylesheets .= $prefix . '<link rel="stylesheet" href="'.$this->assetComposer->getAssetFileName(
+                $stylesheets .= '<link rel="stylesheet" href="'.$this->assetComposer->getAssetFileName(
                     $assetFilename
-                ).'">' . $suffix;
+                ).'">';
             }
         }
 
         return new Markup($stylesheets, 'UTF-8');
     }
 
-    public function renderJavascripts(string $position = 'all', string $prefix = '', string $suffix = ''): Markup
+    public function renderJavascripts(string $position = 'all'): Markup
     {
         $javascripts = '';
         if ((isset($this->assets[$position])) && (isset($this->assets[$position]['js'])) && ([] !== $this->assets[$position]['js'])) {
             foreach ($this->assets[$position]['js'] as $assetFilename) {
-                $javascripts .= $prefix . '<script src="'.$this->assetComposer->getAssetFileName(
+                $javascripts .= '<script src="'.$this->assetComposer->getAssetFileName(
                     $assetFilename
-                ).'"></script>' . $suffix;
+                ).'"></script>';
             }
         }
 
