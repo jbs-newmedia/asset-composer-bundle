2dc8b2bd5c11ef9b3a417e5264fd6ff0d590165f
jschwind <info@juergen-schwind.de>
2024-07-20 17:50:50 +0200

    cqt

--- phpstan-bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!defined('PHPSTAN_RUNNING')) {
-    define('PHPSTAN_RUNNING', true);
-}
-
-function replaceAppPath(string $file): string {
-    return str_replace('/app/', 'var/www/', $file);
-}
